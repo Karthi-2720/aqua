d51a440d7c4236872ae5919c3edd63b33539aeef
Karthi-2720 <24r05a6704@cmrithyderabad.edu.in>
2025-03-22 23:09:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
-
-// Importing express module
 const express = require('express');
 const path = require('path');
 const app = express();
 
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',
-	(req, res) => {
-		res.sendFile(path.join(__dirname, 'public', 'index1.html'));
-	});
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
-app.get('/login',
-	(req, res) => {
-		res.sendFile(path.join(__dirname, 'public', 'login.html'));
-	});
-app.get('/Registration',
-	(req, res) => {
-		res.sendFile(path.join(__dirname, 'public', 'Registration.html'));
-	});
-app.get('/index',
-	(req, res) => {
-		res.sendFile(path.join(__dirname, 'public', 'index.html'));
-	});
+app.get('/Registration', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/Registration.html'));
+});
+
+app.get('/login', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/login.html'));
+});
+
+app.get('/home', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/home.html'));
+});
+
 app.post('/', (req, res) => {
-	res.redirect("/index1")
-});
-app.post('/login', (req, res) => {
-	res.redirect("/login")
-});
-app.post('/register', (req, res) => {
-	res.redirect("/Registration")
-});
-app.post('/index', (req, res) => {
-	res.redirect("/index")
+	res.redirect("/");
 });
 
-app.listen(3000,
-	() => {
-		console.log(
-			'Our express server is up on port 3000'
-		);
-	});
+app.post('/Registration', (req, res) => {
+	res.redirect("/Registration");
+});
+
+app.post('/login', (req, res) => {
+	res.redirect("/login");
+});
+
+app.post('/home', (req, res) => {
+	res.redirect("/home");
+});
+
+app.listen(3000, () => {
+	console.log('Our express server is up on port 3000');
+});
